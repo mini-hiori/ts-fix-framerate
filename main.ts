d0346e0b9caf18326b30308e1303740bc3a8aa04
mini-hiori18 <phantombisco@gmail.com>
2021-05-05 21:24:38 +0900

    target-filesを読み込むように変更

--- main.ts
+++ main.ts
@@ -1,4 +1,5 @@
 import ffmpeg from 'fluent-ffmpeg';
+import * as fs from "fs"
 import * as readline from "readline"
 
 async function fixFramerate(input_filename: string) {
@@ -7,12 +8,11 @@ async function fixFramerate(input_filename: string) {
         const input_fileid = input_filename.replace(".mp4", "");
         const output_filename = input_fileid + "_fixed.mp4";
         const movie = await ffmpeg(input_filename)
-            .inputFPS(60.0)
-            .audioBitrate(192)
-            .audioFrequency(48000)
+            .inputFPS(60.0) // 入力時にfpsを60に固定
+            .audioBitrate(192) // オーディオビットレートを192kbps(元より高め)にする
+            .audioFrequency(48000) // オーディオサンプルレートを48kHzにする(高い)
             .on('end', () => {
                 console.log(`変換終了！${output_filename}が出力されたよ`);
-                console.log("もういっかいやるならファイルパスを入れてね");
             }).save(output_filename);
     } else {
         console.log("ファイル名が怪しいよ");
@@ -20,10 +20,11 @@ async function fixFramerate(input_filename: string) {
 }
 
 async function main() {
-    console.log("ファイルパスをフルパスで入力してね");
-    const event = readline.createInterface({ input: process.stdin });
+    const stream = fs.createReadStream("target_files.txt");
+    const event = readline.createInterface({ input: stream });
     event.on('line', (line) => {
         const input = line.replace("\n", "");
+        console.log(`${input}を処理するよ`);
         fixFramerate(input);
     });
 }
